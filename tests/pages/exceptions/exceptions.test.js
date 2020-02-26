bda8f8193ccb5dc729937b2732878f455001f717
LiShuxue <1149926505@qq.com>
2020-02-26 15:33:51 +0800

    add test for src/pages/exceptions

---
+++ tests/pages/exceptions/exceptions.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Exceptions from 'src/pages/exceptions';
+import { testGetMethod, testPostMethod } from 'src/http/api';
+
+jest.mock('src/http/api', () => ({
+  testGetMethod: jest.fn(() => Promise.resolve('get method success')),
+  testPostMethod: jest.fn(() => Promise.resolve('post method success'))
+}));
+
+test('should render Exceptions page correctly', () => {
+  const testRenderer = renderer.create(<Exceptions />);
+  const dom = testRenderer.toJSON();
+  expect(dom).toMatchSnapshot();
+});
+
+test('should have "testGetApi", "testPostApi" button', () => {
+  const { getByText } = render(<Exceptions />);
+
+  expect.assertions(2);
+
+  const testGetApi = getByText('testGetApi');
+  expect(testGetApi).toBeInTheDocument();
+
+  const testPostApi = getByText('testPostApi');
+  expect(testPostApi).toBeInTheDocument();
+});
+
+test('click "testGetApi" button should call "testGetMethod"', () => {
+  const { getByText } = render(<Exceptions />);
+  const element = getByText('testGetApi');
+  fireEvent.click(element);
+  expect(testGetMethod.mock.calls.length).toEqual(1);
+});
+
+test('click "testPostApi" button should call "testPostMethod"', () => {
+  const { getByText } = render(<Exceptions />);
+  const element = getByText('testPostApi');
+  fireEvent.click(element);
+  expect(testPostMethod.mock.calls.length).toEqual(1);
+});
