18668216885e61e42d33477198aa813fade7f573
LiShuxue <1149926505@qq.com>
2020-02-28 16:56:34 +0800

    use react-redux hooks to refactor the Rules page

--- src/pages/rules/index.tsx
+++ src/pages/rules/index.tsx
@@ -1,19 +1,66 @@
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { actionCreatorA, asyncActionCreatorB } from '../../store/actions';
+
+// export const Rules: React.FunctionComponent = (props: any) => {
+//   const { stateA, stateB, actionCreatorA, asyncActionCreatorB } = props;
+//   const buttonAction2 = () => {
+//     // can do more thing
+//     asyncActionCreatorB();
+//   };
+
+//   return (
+//     <div>
+//       <h1>Rules</h1>
+//       <hr />
+//       <button onClick={() => actionCreatorA('changed')}>click this button to change the stateA</button>
+//       <br />
+//       <h1>stateA: {stateA}</h1>
+//       <br />
+//       <button onClick={buttonAction2}>click this button to async change the stateB</button>
+//       <br />
+//       <h1>stateB: {stateB}</h1>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = (state: any) => {
+//   let { stateA, stateB } = state;
+//   return {
+//     stateA,
+//     stateB
+//   };
+// };
+
+// const mapDispatchToProps = {
+//   actionCreatorA,
+//   asyncActionCreatorB
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Rules);
+
+/**
+ *  use react-redux hooks to refactor the above code
+ */
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { actionCreatorA, asyncActionCreatorB } from '../../store/actions';
 
-export const Rules: React.FunctionComponent = (props: any) => {
-  const { stateA, stateB, actionCreatorA, asyncActionCreatorB } = props;
+const Rules: React.FC = (props: any) => {
+  const stateA = useSelector((state: any) => state.stateA);
+  const stateB = useSelector((state: any) => state.stateB);
+  const dispatch = useDispatch();
+
   const buttonAction2 = () => {
     // can do more thing
-    asyncActionCreatorB('async changed');
+    dispatch(asyncActionCreatorB());
   };
 
   return (
     <div>
       <h1>Rules</h1>
       <hr />
-      <button onClick={() => actionCreatorA('changed')}>click this button to change the stateA</button>
+      <button onClick={() => dispatch(actionCreatorA('changed'))}>click this button to change the stateA</button>
       <br />
       <h1>stateA: {stateA}</h1>
       <br />
@@ -24,17 +71,4 @@ export const Rules: React.FunctionComponent = (props: any) => {
   );
 };
 
-const mapStateToProps = (state: any) => {
-  let { stateA, stateB } = state;
-  return {
-    stateA,
-    stateB
-  };
-};
-
-const mapDispatchToProps = {
-  actionCreatorA,
-  asyncActionCreatorB
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Rules);
+export default Rules;
