e2e40743907847fac57f138a9b1d81bfd54d56df
LiShuxue <1149926505@qq.com>
2020-03-09 09:27:08 +0800

    add test for switch theme component

---
+++ tests/components/switchTheme/switchTheme.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import SwitchTheme from 'components/switchTheme';
+
+test('should render Header component correctly', () => {
+  const { asFragment } = render(<SwitchTheme />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('should have "dark theme" button and click this button should change to dark theme', () => {
+  const { getByText } = render(<SwitchTheme />);
+
+  const darkThemeBtn = getByText('dark theme');
+  expect(darkThemeBtn).toBeInTheDocument();
+
+  fireEvent.click(darkThemeBtn);
+  expect(localStorage.getItem('ratan-theme')).toEqual('dark');
+});
+
+test('should have "light theme" button and click this button should change to light theme', () => {
+  const { getByText } = render(<SwitchTheme />);
+
+  const lightThemeBtn = getByText('light theme');
+  expect(lightThemeBtn).toBeInTheDocument();
+
+  fireEvent.click(lightThemeBtn);
+  expect(localStorage.getItem('ratan-theme')).toEqual('light');
+});
